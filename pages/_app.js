821cabd427894eb4262c2775cc6c1eaf4a64eeb8
Jammer0n <ijsamuel@gmail.com>
2023-02-23 15:07:43 -0500

    revert

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,12 @@
-import '../styles/globals.css'
-import Layout from '../components/Layout'
+import "../styles/globals.css";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
 
-export default MyApp
-
-PC.init = function () {
-  // Initializes the Contentful client with the required credentials for a
-  // product catalog space. Feel free to replace them with our own.
-  PC.contentfulClient = contentful.createClient({
-    accessToken: 'abcd1234abcd1234abcd1234abcd1234abcd1234abcd1234abcd1234abcd1234abc', // replace the access token
-    space: 'klmno09876klmno' // replace the space ID
-  })
+export default MyApp;
